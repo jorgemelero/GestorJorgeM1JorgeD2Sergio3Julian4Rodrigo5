52533fb42cfe1ceeb669bfb311a61df4417c7e85
jorgemelero <35265744+jorgemelero@users.noreply.github.com>
2018-01-24 20:09:19 +0100

    Add files via upload
    He añadido los metodos setEscenarioDetalle y setComponente.

---
+++ src/view/VistaDetalleController.java
@@ -0,0 +1,68 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package view;
+
+import controller.GestorInventario;
+import javafx.fxml.FXML;
+import javafx.scene.control.Label;
+import javafx.stage.Stage;
+import modelo.Componente;
+
+/**
+ *
+ * @author milopez
+ */
+public class VistaDetalleController {
+
+    @FXML
+    public Label nombreLabel;
+    @FXML
+    private Label precioLabel;
+    @FXML
+    private Label stockLabel;
+    @FXML
+    private Label fechaModificacionLabel;
+    @FXML
+    private Label fechaAltaLabel;
+    @FXML
+    private Label descripcionLabel;
+
+    private GestorInventario gestorInventario;
+    private Componente componente;
+    private VistaComponenteController datos;
+    private Stage escenarioDetalle;
+
+   
+     public VistaDetalleController() {
+  //   this.componente = componente;
+//     nombre= componente.getNombre();
+      
+    }
+    @FXML
+    public void initialize(){
+//        nombreLabel.setText(datos.getNombre(componente));
+//        precioLabel.setText(componente.getPrecio());
+//        stockLabel.setText(componente.getStock());
+//        gestorInventario.muestraDetalle();
+    }
+  
+    public void setEscenarioDetalle(Stage escenarioDetalle){
+        this.escenarioDetalle = escenarioDetalle;
+    }
+    
+    public void setComponente(Componente componente){
+        this.componente = componente;
+        
+        nombreLabel.setText(componente.getNombre());
+        precioLabel.setText(componente.getPrecio());
+        stockLabel.setText(componente.getStock());
+        
+//        fechaModificacionLabel.setText(componente.getNombre());
+//        fechaAltaLabel.setText(componente.getNombre());
+//        descripcionLabel.setText(componente.getNombre());
+    }
+
+}
