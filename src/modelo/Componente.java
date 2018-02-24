d196803da6f29320ade39bd9d3035581948b1a4f
Rod180 <35265776+Rod180@users.noreply.github.com>
2018-02-24 20:48:12 +0100

    Add files via upload
    modelo con todos los cambios

--- src/modelo/Componente.java
+++ src/modelo/Componente.java
@@ -5,10 +5,17 @@
  */
 package modelo;
 
+import java.time.LocalDate;
+import java.time.Month;
+import javafx.beans.property.ObjectProperty;
+import javafx.beans.property.SimpleObjectProperty;
 import javafx.beans.property.SimpleStringProperty;
 import javafx.beans.property.StringProperty;
 import javafx.scene.image.Image;
 import javafx.scene.image.ImageView;
+import javax.xml.bind.annotation.adapters.XmlJavaTypeAdapter;
+import util.Adaptador;
+import util.AdaptadorFoto;
 
 /**
  *
@@ -20,25 +27,55 @@ public class Componente {
     private final StringProperty nombre;
     private final StringProperty precio;
     private final StringProperty stock;
-    private final String ruta;
+    private LocalDate fechaAlta;
+    private LocalDate fechaMod;
+    private String ruta;
+    private String descripcion;
+    private Integer codigo;
     
     public Componente(){
-        this(null,null,null,null,null);
+        this(null,null,null,null,null,null,null,null,null);
     }
 
-    public Componente(ImageView foto, String nombre, String precio, String stock, String ruta) {
+    public Componente(ImageView foto, String nombre, String precio, String stock, String ruta, 
+                      LocalDate fechaAlta,LocalDate fechaMod, Integer codigo,String descripcion) {
         this.foto = (ImageView)foto;
         this.nombre = new SimpleStringProperty(nombre);
         this.precio = new SimpleStringProperty(precio);
         this.stock = new SimpleStringProperty(stock);
+        this.fechaAlta = fechaAlta;
+        this.fechaMod = fechaMod;
         this.ruta = ruta;
+        this.descripcion = descripcion;
+        this.codigo = codigo;
 
     }
+
+    public String getDescripcion() {
+        return descripcion;
+    }
+
+    public void setDescripcion(String descripcion) {
+        this.descripcion = descripcion;
+    }
+
+    public Integer getCodigo() {
+        return codigo;
+    }
+
+    public void setCodigo(Integer codigo) {
+        this.codigo = codigo;
+    }
+
+   
 
     public String getRuta() {
         return ruta;
     }
-
+     public void setRuta(String ruta) {
+        this.ruta = ruta;
+    }
+     @XmlJavaTypeAdapter(AdaptadorFoto.class)
     public ImageView getFoto() {
         return foto;
     }
@@ -82,5 +119,25 @@ public class Componente {
     public void setStock(String stock) {
         this.stock.set(stock);
     }
+    
+    @XmlJavaTypeAdapter(Adaptador.class)
+    public LocalDate getFechaAlta() {
+        return fechaAlta;
+    }
+    @XmlJavaTypeAdapter(Adaptador.class)
+    public LocalDate getFechaMod() {
+        return fechaMod;
+    }
+      public LocalDate setFechaAlta(LocalDate fecha) {
+        fechaAlta = fecha;
+        return fechaAlta;
+    }
+    
+     public LocalDate setFechaMod(LocalDate fecha) {
+         fechaMod = fecha;
+        return fechaMod;
+    }
+     
+    
 
 }
